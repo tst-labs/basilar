00a3db002540b8e02f70181576ed3772947bac1b
Adriano Lemos <adriano.lemos.dev@gmail.com>
2022-05-03 18:32:50 -0300

    feat: Configuração inicial com Material-UI e Storybook

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import tstTheme from './tst-theme';
+
+addons.setConfig({
+  theme: tstTheme,
+});
---
+++ src/componentes/ola-mundo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OlaMundo = () => {
+    return <h1>Olá, mundo maravilhoso!!!</h1>
+};
+
+export default OlaMundo;
