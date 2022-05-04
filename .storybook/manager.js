64d94dbddea8323e620a85ac1b420aaaf1aafed1
adriano-lemos-dev <adriano.lemos.dev@gmail.com>
2022-05-04 21:00:30 +0000

    Deploying to gh-pages from  @ f45cd16c52d4455943c3ad9e9a17165c809baad7 🚀

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import tstTheme from './tst-theme';
-
-addons.setConfig({
-  theme: tstTheme,
-});
--- src/index.js
+++
@@ -1,2 +0,0 @@
-export { default as OlaMundo } from './componentes/ola-mundo.jsx';
-export { Button } from './componentes/Button.jsx';
