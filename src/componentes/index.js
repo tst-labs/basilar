2e7cedc65e774457848c6fa0da178ec6dc1ef573
Adriano Lemos <adriano.lemos.dev@gmail.com>
2022-05-06 07:23:57 -0300

    feat: Cria um componente Provider (provedor) do tema do TST.
    Um facilitador, caso o desenvolvedor não deseje alterar o tema.

--- src/componentes/index.js
+++ src/componentes/index.js
@@ -1,3 +1,3 @@
 export { default as Botao } from "./botao/index";
 export { default as OlaMundo } from "./alo-mundo/index";
-export { default as temaTST } from "./tema/index";
+export { temaTST, ProvedorTemaTST } from "./tema/index";
