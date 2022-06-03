a46ae7284f8b13bc9b127f78ffe74119188696f2
Adriano Lemos <adriano.lemos.dev@gmail.com>
2022-06-03 16:52:54 -0300

    ci: Cria a action de publicação da biblioteca na registry do NPM.

--- src/componentes/logos/index.js
+++ src/componentes/logos/index.js
@@ -1,14 +1,13 @@
-// export { LogoBicolor } from "./logo-bicolor/index";
 import React from 'react';
 import { CustomSVG } from './custom-svg/index';
 import LogosBase from './logos-base';
 
-const Logo = ({largura}) => <CustomSVG {...LogosBase('Logo')} largura={largura} />;
-const LogoBicolor = ({largura}) => <CustomSVG {...LogosBase('LogoBicolor')} largura={largura} />;
-const LogoMonoBranco = ({largura}) => <CustomSVG {...LogosBase('LogoMonoBranco')} largura={largura} />;
-const LogoMonoPreto = ({largura}) => <CustomSVG {...LogosBase('LogoMonoPreto')} largura={largura} />;
-const LogoMonoAzul = ({largura}) => <CustomSVG {...LogosBase('LogoMonoAzul')} largura={largura} />;
-const LogoTST = ({largura}) => <CustomSVG {...LogosBase('LogoTST')} largura={largura} />;
-const LogoTSTMonoBicolor = ({largura}) => <CustomSVG {...LogosBase('LogoTSTMonoBicolor')} largura={largura} />;
+const Logo                = ({largura}) => <CustomSVG {...LogosBase('Logo')} largura={largura} />;
+const LogoBicolor         = ({largura}) => <CustomSVG {...LogosBase('LogoBicolor')} largura={largura} />;
+const LogoMonoBranco      = ({largura}) => <CustomSVG {...LogosBase('LogoMonoBranco')} largura={largura} />;
+const LogoMonoPreto       = ({largura}) => <CustomSVG {...LogosBase('LogoMonoPreto')} largura={largura} />;
+const LogoMonoAzul        = ({largura}) => <CustomSVG {...LogosBase('LogoMonoAzul')} largura={largura} />;
+const LogoTST             = ({largura}) => <CustomSVG {...LogosBase('LogoTST')} largura={largura} />;
+const LogoTSTMonoBicolor  = ({largura}) => <CustomSVG {...LogosBase('LogoTSTMonoBicolor')} largura={largura} />;
 
 export { Logo, LogoBicolor, LogoMonoBranco, LogoMonoPreto, LogoMonoAzul, LogoTST, LogoTSTMonoBicolor };
