42977ec06303ff47a4f0f53e425471c29c276a02
Adriano Lemos <adriano.lemos.dev@gmail.com>
2022-06-15 08:55:26 -0300

    refactor: Usa SVGR para criar componentes de Logos.
    Reorganiza os exports e remove componentes antigos.

--- src/componentes/logos/index.js
+++ src/componentes/logos/index.js
@@ -1,13 +1,72 @@
 import React from 'react';
-import { CustomSVG } from './custom-svg/index';
-import LogosBase from './logos-base';
 
-const Logo                = ({largura}) => <CustomSVG {...LogosBase('Logo')} largura={largura} />;
-const LogoBicolor         = ({largura}) => <CustomSVG {...LogosBase('LogoBicolor')} largura={largura} />;
-const LogoMonoBranco      = ({largura}) => <CustomSVG {...LogosBase('LogoMonoBranco')} largura={largura} />;
-const LogoMonoPreto       = ({largura}) => <CustomSVG {...LogosBase('LogoMonoPreto')} largura={largura} />;
-const LogoMonoAzul        = ({largura}) => <CustomSVG {...LogosBase('LogoMonoAzul')} largura={largura} />;
-const LogoTST             = ({largura}) => <CustomSVG {...LogosBase('LogoTST')} largura={largura} />;
-const LogoTSTMonoBicolor  = ({largura}) => <CustomSVG {...LogosBase('LogoTSTMonoBicolor')} largura={largura} />;
+import { ReactComponent as Logo } from './svgs/logo.svg';
+import { ReactComponent as LogoBicolor } from './svgs/logo_bicolor.svg';
+import { ReactComponent as LogoMonoBranco } from './svgs/logo_mono_branco.svg';
+import { ReactComponent as LogoMonoPreto } from './svgs/logo_mono_preto.svg';
+import { ReactComponent as LogoMonoAzul } from './svgs/logo_mono_azul.svg';
+import { ReactComponent as LogoSimplificado } from './svgs/logo_simplificado.svg';
 
-export { Logo, LogoBicolor, LogoMonoBranco, LogoMonoPreto, LogoMonoAzul, LogoTST, LogoTSTMonoBicolor };
+import { ReactComponent as LogoJTTST } from './svgs/logo_jt_tst.svg';
+import { ReactComponent as LogoJTTSTBicolor } from './svgs/logo_jt_tst_bicolor.svg';
+import { ReactComponent as LogoJTTSTMonoAzul } from './svgs/logo_jt_tst_mono_azul.svg';
+import { ReactComponent as LogoJTTSTMonoBranco } from './svgs/logo_jt_tst_mono_branco.svg';
+import { ReactComponent as LogoJTTSTMonoPreto } from './svgs/logo_jt_tst_mono_preto.svg';
+import { ReactComponent as LogoJTTSTSimplificado } from './svgs/logo_jt_tst_simplificado.svg';
+
+import { ReactComponent as LogoJTTST2 } from './svgs/logo_jt_tst2.svg';
+import { ReactComponent as LogoJTTST2Bicolor } from './svgs/logo_jt_tst2_bicolor.svg';
+import { ReactComponent as LogoJTTST2MonoAzul } from './svgs/logo_jt_tst2_mono_azul.svg';
+import { ReactComponent as LogoJTTST2MonoBranco } from './svgs/logo_jt_tst2_mono_branco.svg';
+import { ReactComponent as LogoJTTST2MonoPreto } from './svgs/logo_jt_tst2_mono_preto.svg';
+import { ReactComponent as LogoJTTST2Simplificado } from './svgs/logo_jt_tst2_simplificado.svg';
+
+import { ReactComponent as LogoTST } from './svgs/logo_tst.svg';
+import { ReactComponent as LogoTSTMonoBicolor } from './svgs/logo_tst_mono_bicolor.svg';
+import { ReactComponent as LogoTSTMonoAzul } from './svgs/logo_tst_mono_azul.svg';
+import { ReactComponent as LogoTSTMonoBranco } from './svgs/logo_tst_mono_branco.svg';
+import { ReactComponent as LogoTSTMonoPreto } from './svgs/logo_tst_mono_preto.svg';
+import { ReactComponent as LogoTSTSimplificado } from './svgs/logo_tst_simplificado.svg';
+import { ReactComponent as LogoTSTVertical } from './svgs/logo_tst_vertical.svg';
+
+import { ReactComponent as LogoTSTExt } from './svgs/logo_tribunal_superior_do_trabalho.svg';
+import { ReactComponent as LogoTSTBicolorExt } from './svgs/logo_tribunal_superior_do_trabalho_bicolor.svg';
+import { ReactComponent as LogoTSTMonoAzulExt } from './svgs/logo_tribunal_superior_do_trabalho_mono_azul.svg';
+import { ReactComponent as LogoTSTMonoBrancoExt } from './svgs/logo_tribunal_superior_do_trabalho_mono_branco.svg';
+import { ReactComponent as LogoTSTMonoPretoExt } from './svgs/logo_tribunal_superior_do_trabalho_mono_preto.svg';
+import { ReactComponent as LogoTSTSimplificadoExt } from './svgs/logo_tribunal_superior_do_trabalho_simplificado.svg';
+
+
+export {
+  Logo,
+  LogoBicolor,
+  LogoMonoBranco,
+  LogoMonoPreto,
+  LogoMonoAzul,
+  LogoSimplificado,
+  LogoJTTST,
+  LogoJTTSTBicolor,
+  LogoJTTSTMonoAzul,
+  LogoJTTSTMonoBranco,
+  LogoJTTSTMonoPreto,
+  LogoJTTSTSimplificado,
+  LogoJTTST2,
+  LogoJTTST2Bicolor,
+  LogoJTTST2MonoAzul,
+  LogoJTTST2MonoBranco,
+  LogoJTTST2MonoPreto,
+  LogoJTTST2Simplificado,
+  LogoTST,
+  LogoTSTMonoBicolor,
+  LogoTSTMonoAzul,
+  LogoTSTMonoBranco,
+  LogoTSTMonoPreto,
+  LogoTSTSimplificado,
+  LogoTSTVertical,
+  LogoTSTExt,
+  LogoTSTMonoAzulExt,
+  LogoTSTBicolorExt,
+  LogoTSTMonoBrancoExt,
+  LogoTSTMonoPretoExt,
+  LogoTSTSimplificadoExt
+};
