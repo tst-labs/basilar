8f02f0a6c6716a24ea9aa15cfc63ea827a1887c7
Rodrigo Cardoso Vieira <rodrigo.vieira@tst.jus.br>
2022-08-29 15:24:46 -0300

    fix #18 - Corrige o warning

--- src/componentes/loading-provider/loading-provider.test.jsx
+++ src/componentes/loading-provider/loading-provider.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { act, render, screen, waitFor } from "@testing-library/react";
 import Button from "@mui/material/Button";
 
 import { LoadingProvider, useLoading } from "./index";
@@ -27,29 +27,25 @@ describe("Testes do Loading Provider", () => {
     expect(ComponenteFilho).toBeVisible();
   });
 
-  /**
-   * Os testes passam, mas esse warning aparece no console
-   * Warning: Functions are not valid as a React child.
-   * This may happen if you return a Component instead of <Component />
-   * from render. Or maybe you meant to call this function rather than
-   * return it.
-   */
   test("Deve Exibir o loading provider", () => {
-    const Componente = () => {
+    const ComponenteFilho = () => {
       const setLoading = useLoading(false);
-      setLoading(true);
-      return <></>;
+      return <button onClick={() => setLoading(true)}>Filho</button>;
     };
 
-    render(() => {
+    render(
       <LoadingProvider>
-        <Componente />
-      </LoadingProvider>;
+        <ComponenteFilho />
+      </LoadingProvider>
+    );
+
+    const progress = screen.queryByTestId("loader");
+    const botao = screen.queryByText("Filho");
+    act(() => {
+      botao.click();
     });
 
-    const progress = screen.findByTestId("loader");
-
-    waitFor(() => expect(progress).toBeInTheDocument());
-    waitFor(() => expect(progress).toBeVisible());
+    expect(progress).toBeInTheDocument();
+    expect(progress).toBeVisible();
   });
 });
