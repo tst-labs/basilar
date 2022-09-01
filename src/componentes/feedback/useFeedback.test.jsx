e256a9f2302170cf72db3f1de7ded9c0aff3abe1
Rodrigo Cardoso Vieira <rodrigo.vieira@tst.jus.br>
2022-09-01 08:24:12 -0300

    Completa os testes de useFeedback

--- src/componentes/feedback/useFeedback.test.jsx
+++ src/componentes/feedback/useFeedback.test.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-import {
-  act,
-  render,
-  screen,
-  waitForElementToBeRemoved
-} from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Button from "@mui/material/Button";
+import { Button, Chip } from "@material-ui/core";
 import { SnackbarProvider } from "notistack";
 
 import { useFeedback } from "./index";
 
 describe("Testes do useFeedback", () => {
   test("Deve exibir o feedback de sucesso", () => {
-    const ComponenteFilho = () => {
+    const MostraMensagemSimples = () => {
       const { showSuccess } = useFeedback();
       return (
         <Button onClick={() => showSuccess("Mensagem de Sucesso")}>
@@ -24,7 +19,7 @@ describe("Testes do useFeedback", () => {
 
     render(
       <SnackbarProvider maxSnack={1}>
-        <ComponenteFilho />
+        <MostraMensagemSimples />
       </SnackbarProvider>
     );
 
@@ -38,7 +33,7 @@ describe("Testes do useFeedback", () => {
   });
 
   test("Deve exibir o feedback de info", () => {
-    const ComponenteFilho = () => {
+    const MostraMensagemInfo = () => {
       const { showInfo } = useFeedback();
       return (
         <Button onClick={() => showInfo("Mensagem Informativa")}>
@@ -49,7 +44,7 @@ describe("Testes do useFeedback", () => {
 
     render(
       <SnackbarProvider maxSnack={1}>
-        <ComponenteFilho />
+        <MostraMensagemInfo />
       </SnackbarProvider>
     );
 
@@ -63,7 +58,7 @@ describe("Testes do useFeedback", () => {
   });
 
   test("Deve exibir o feedback de erro", () => {
-    const ComponenteFilho = () => {
+    const MostraMensagemErro = () => {
       const { showError } = useFeedback();
       return (
         <Button onClick={() => showError("Mensagem de Erro")}>
@@ -74,7 +69,7 @@ describe("Testes do useFeedback", () => {
 
     render(
       <SnackbarProvider maxSnack={1}>
-        <ComponenteFilho />
+        <MostraMensagemErro />
       </SnackbarProvider>
     );
 
@@ -88,7 +83,7 @@ describe("Testes do useFeedback", () => {
   });
 
   test("Deve exibir o feedback de alerta", () => {
-    const ComponenteFilho = () => {
+    const MostraMensagemAlerta = () => {
       const { showWarning } = useFeedback();
       return (
         <Button onClick={() => showWarning("Mensagem de alerta")}>
@@ -99,7 +94,7 @@ describe("Testes do useFeedback", () => {
 
     render(
       <SnackbarProvider maxSnack={1}>
-        <ComponenteFilho />
+        <MostraMensagemAlerta />
       </SnackbarProvider>
     );
 
@@ -110,5 +105,136 @@ describe("Testes do useFeedback", () => {
     const feedback = screen.queryByText("Mensagem de alerta");
     expect(feedback).toBeInTheDocument();
     expect(feedback).toBeVisible();
+  });
+
+  test("Deve exibir o feedback de sucesso com elemento interno", () => {
+    const MostraMensagemSimples = () => {
+      const { showSuccess } = useFeedback();
+      return (
+        <Button onClick={() => showSuccess(<Chip label="Elemento" />)}>
+          Mostra Mensagem
+        </Button>
+      );
+    };
+
+    render(
+      <SnackbarProvider maxSnack={1}>
+        <MostraMensagemSimples />
+      </SnackbarProvider>
+    );
+
+    act(() => {
+      userEvent.click(screen.getByText("Mostra Mensagem"));
+    });
+
+    const feedback = screen.queryByText("Elemento");
+    expect(feedback).toBeInTheDocument();
+    expect(feedback).toBeVisible();
+  });
+
+  test("Deve exibir o feedback de sucesso com título e corpo", () => {
+    const MostraMensagemSimples = () => {
+      const { showSuccess } = useFeedback();
+      return (
+        <Button onClick={() => showSuccess({ title: "titulo", body: "corpo" })}>
+          Mostra Mensagem
+        </Button>
+      );
+    };
+
+    render(
+      <SnackbarProvider maxSnack={1}>
+        <MostraMensagemSimples />
+      </SnackbarProvider>
+    );
+
+    act(() => {
+      userEvent.click(screen.getByText("Mostra Mensagem"));
+    });
+
+    const titulo = screen.queryByText("titulo");
+    expect(titulo).toBeInTheDocument();
+    expect(titulo).toBeVisible();
+
+    const corpo = screen.queryByText("corpo");
+    expect(corpo).toBeInTheDocument();
+    expect(corpo).toBeVisible();
+  });
+
+  test("Deve exibir o feedback de sucesso com lista", () => {
+    const MostraMensagemSimples = () => {
+      const { showSuccess } = useFeedback();
+      return (
+        <Button
+          onClick={() =>
+            showSuccess({
+              title: "titulo",
+              bodyList: ["Mensagem 1", "Mensagem 2"]
+            })
+          }
+        >
+          Mostra Mensagem
+        </Button>
+      );
+    };
+
+    render(
+      <SnackbarProvider maxSnack={1}>
+        <MostraMensagemSimples />
+      </SnackbarProvider>
+    );
+
+    act(() => {
+      userEvent.click(screen.getByText("Mostra Mensagem"));
+    });
+
+    const titulo = screen.queryByText("titulo");
+    expect(titulo).toBeInTheDocument();
+    expect(titulo).toBeVisible();
+
+    const mensagem1 = screen.queryByText("Mensagem 1");
+    expect(mensagem1).toBeInTheDocument();
+    expect(mensagem1).toBeVisible();
+
+    const mensagem2 = screen.queryByText("Mensagem 2");
+    expect(mensagem2).toBeInTheDocument();
+    expect(mensagem2).toBeVisible();
+  });
+
+  test("Deve exibir o feedback de erro de api", () => {
+    const MostraMensagemSimples = () => {
+      const { showError } = useFeedback();
+      const apiError = {
+        message: "Este é o formato de erro esperado da API",
+        errors: [{ message: "Detalhe 1" }, { defaultMessage: "Detalhe 2" }]
+      };
+      return (
+        <Button onClick={() => showError(apiError)}>Mostra Mensagem</Button>
+      );
+    };
+
+    render(
+      <SnackbarProvider maxSnack={1}>
+        <MostraMensagemSimples />
+      </SnackbarProvider>
+    );
+
+    act(() => {
+      userEvent.click(screen.getByText("Mostra Mensagem"));
+    });
+
+    const titulo = screen.queryByText(
+      "Este é o formato de erro esperado da API"
+    );
+    expect(titulo).toBeInTheDocument();
+    expect(titulo).toBeVisible();
+
+    const mensagem = screen.queryByText("Detalhe 1");
+    expect(mensagem).toBeInTheDocument();
+    expect(mensagem).toBeVisible();
+
+    const mensagem2 = screen.queryByText("Detalhe 2");
+    expect(mensagem2).toBeInTheDocument();
+    expect(mensagem2).toBeVisible();
   });
 });
