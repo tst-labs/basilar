f34dd9ca967da5bae2c51dd20dd728357931f736
Rodrigo Cardoso Vieira <rodrigo.vieira@tst.jus.br>
2022-09-01 17:00:01 -0300

    Inclui o componente TextLoading

---
+++ src/componentes/text-loading/loading-text.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { LoadingText } from "./index";
+import Button from "@mui/material/Button";
+
+export default {
+  title: "Basilar/Componentes/LoadingText",
+  component: LoadingText
+};
+
+export const Default = () => {
+  return <LoadingText>Este é um texto de exemplo</LoadingText>;
+};
+
+export const H1 = () => {
+  return <LoadingText variant="h1">Este é um texto de exemplo</LoadingText>;
+};
