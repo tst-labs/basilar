32c0e3b9abc8d2267a68990187b683896609f58d
Rodrigo Cardoso Vieira <rodrigo.vieira@tst.jus.br>
2022-09-02 08:46:43 -0300

    Substitui uso do @material pelo @mui/material em loadingProvider

--- src/componentes/loading-provider/loading-provider.js
+++ src/componentes/loading-provider/loading-provider.js
@@ -1,22 +1,19 @@
-import { Backdrop, CircularProgress, makeStyles } from "@material-ui/core";
 import React, { createContext, useContext, useState } from "react";
+import { Backdrop, CircularProgress } from "@mui/material";
 
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.modal + 1,
-    color: "#fff"
-  }
-}));
+const style = {
+  zIndex: (theme) => theme.zIndex.drawer + 1,
+  color: "#fff"
+};
 
 const LoadingContext = createContext();
 
 export function LoadingProvider({ children }) {
   const [loading, setLoading] = useState(false);
-  const classes = useStyles();
 
   return (
     <LoadingContext.Provider value={setLoading}>
-      <Backdrop className={classes.backdrop} open={loading}>
+      <Backdrop sx={style} open={loading}>
         <CircularProgress color="inherit" data-testid="loader" />
       </Backdrop>
       {children}
